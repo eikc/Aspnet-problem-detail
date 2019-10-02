370ad896a7bede204e21a2665e98f2dee15721d7
Eik Emil Christensen <eic@tdc.dk>
2019-10-02 09:57:37 +0200

    init
    .

---
+++ WebApplication/Controllers/WeatherForecastController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Problem("Should return 400 bad request", statusCode: StatusCodes.Status400BadRequest);
+        }
+    }
+}
